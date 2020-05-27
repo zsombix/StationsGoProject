4698ed3540e5a7265d5b81c471bbaa875c34fff0
Zsombor Vajda <zsombor.vajda@outlook.com>
2020-05-27 13:18:34 +0200

    A Working Web Server that returns a response

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/zsombix/StationsGoProject/stations"
 )
 
 func main() {
 
-	fmt.Println(stations.Stations)
+	http.HandleFunc("/stations", getStations)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func getStations(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(&stations.Stations)
+	if err != nil {
+		log.Fatal("Could not convert")
+	}
+	//Add the proper header values
+	header := w.Header()
+	header.Add("Content-Type", "application/json")
+
+	//Write response
+	fmt.Fprintf(w, string(data))
+}
+
+//This is just an alternative for Encoding to JSON
+func tempGetStations(w http.ResponseWriter, r *http.Request) {
+	var buffer bytes.Buffer
+	json.NewEncoder(&buffer).Encode(&stations.Stations)
+
+	fmt.Fprintf(w, buffer.String())
 }
--- stations/stations.go
+++ stations/stations.go
@@ -2,6 +2,7 @@ package stations
 
 //Station that is used in the project
 type Station struct {
+	ID        int32
 	Name      string
 	Latitude  float64
 	Longitude float64
@@ -9,19 +10,22 @@ type Station struct {
 
 //Stations is a list of available stations
 var Stations [3]Station = [3]Station{
-	Station{
+	{
+		ID:        1,
 		Name:      "Budapest",
-		Latitude:  12.3333,
-		Longitude: 43.123,
+		Latitude:  47.497913,
+		Longitude: 19.040236,
 	},
-	Station{
-		Name:      "Budapest",
-		Latitude:  12.3333,
-		Longitude: 43.123,
+	{
+		ID:        2,
+		Name:      "London",
+		Latitude:  51.507351,
+		Longitude: -0.127758,
 	},
-	Station{
-		Name:      "Budapest",
-		Latitude:  12.3333,
-		Longitude: 43.123,
+	{
+		ID:        3,
+		Name:      "Bucharest",
+		Latitude:  44.426765,
+		Longitude: 26.102537,
 	},
 }
